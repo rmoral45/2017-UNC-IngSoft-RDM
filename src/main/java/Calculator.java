836d54fdd8d5f80285d65e5f06f9f4a05795e225
rmoral45 <rmoral45@gmail.com>
2017-04-01 20:01:02 -0300

    Testeo como en clase

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+package main.java;
+
+/**
+ * Created by Ramiro on 01/04/2017.
+ */
+public class Calculator {
+    public static void Calculator(){
+    }
+
+    int suma(int x, int y){
+        return x+y;
+    }
+}
