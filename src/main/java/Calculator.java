fbd148b9b6e3f6e61d70da65837c08e3fa8651e0
rmoral45 <rmoral45@gmail.com>
2017-04-02 21:35:31 -0300

    assert tutorial

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -10,4 +10,12 @@ public class Calculator {
     int suma(int x, int y){
         return x+y;
     }
+
+    int producto(int x, int y){
+        return x*y;
+    }
+
+    int division(int x, int y){
+        return x/y;
+    }
 }
