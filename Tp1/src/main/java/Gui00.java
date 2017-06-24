57b8ddf8d1e9b6fe980c787ff1fda5bbf2844510
Ramiro <rmoral45@gmail.com>
2017-06-24 09:58:48 -0300

    creando GUI

---
+++ Tp1/src/main/java/Gui00.java
@@ -0,0 +1,15 @@
+package java;
+import javax.swing.*;
+
+/**
+ * Created by Ramiro on 24/6/2017.
+ */
+public class Gui00 extends JFrame{
+
+    public Gui00(){
+
+        super("Don't Forget It");
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
--- Tp1/src/main/java/Main.java
+++ Tp1/src/main/java/Main.java
@@ -1,7 +1,11 @@
 package java;
 
+import javax.swing.*;
 /**
  * Created by Ramiro on 23/05/2017.
  */
 public class Main {
+    public static void main(String[] args){
+        Gui00 DontFIt = new Gui00();
+    }
 }
